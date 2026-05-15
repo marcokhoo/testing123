8c87f40f3bf014b2e6d5d090205c07c80dba370c
Marcokhoo <marcokhoo@computime.com>
2026-05-15 11:12:49 +0800

    branch test 123abc

--- frdmmcxa156_hello_world137/hello_world.c
+++ frdmmcxa156_hello_world137/hello_world.c
@@ -38,7 +38,7 @@ int main(void)
 
     PRINTF("MCUX SDK version: %s\r\n", MCUXSDK_VERSION_FULL_STR);
 
-    PRINTF("hello world_test123.\r\n");
+    PRINTF("hello world_test123ABC.\r\n");
 
     while (1)
     {
